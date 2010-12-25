0d4babc163160f3d953d60705b42e0e5af0b53ca
Michael Patricios <michael.patricios@gmail.com>
2010-12-25 18:03:44 +0000

    Tidying up

--- main.m
+++ main.m
@@ -1,14 +1,5 @@
-//
-//  main.m
-//  SimpleViewController2
-//
-//  Created by Cathy Shive on 7/21/08.
-//  Copyright __MyCompanyName__ 2008. All rights reserved.
-//
-
 #import <Cocoa/Cocoa.h>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     return NSApplicationMain(argc,  (const char **) argv);
 }
